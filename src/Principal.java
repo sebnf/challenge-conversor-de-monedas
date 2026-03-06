13c39c5b39eb97096f1473861fb61eb8fa4ca8d9
sebnf <sebastian.flores.acevedo@gmail.com>
2026-03-06 01:38:33 -0300

    Se crea el proyecto y se configura gson

---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+import com.google.gson.Gson;
+
+public class Principal {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        System.out.println("¡Perfecto! Gson está funcionando.");
+    }
+}
